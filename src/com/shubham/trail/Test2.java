ea50e30122716b8c09ab53fde59344610229b00b
Shubham Kamal <shubhamkamal44@gmail.com>
2022-10-27 21:47:47 +0530

    First Commit without sample

--- src/com/shubham/trail/Test2.java
+++ src/com/shubham/trail/Test2.java
@@ -1,5 +1,30 @@
 package com.shubham.trail;
 
+import java.util.*;
+
 public class Test2 {
 
+    public static void main(String[] args) {
+
+        List<Integer> integerList1 = new ArrayList<>();
+        integerList1.add(1);
+        integerList1.add(2);
+        integerList1.add(3);
+        integerList1.add(4);
+        integerList1.add(5);
+
+        List<Integer> integerList2 = new ArrayList<>();
+        integerList2.add(6);
+        integerList2.add(2);
+        integerList2.add(7);
+        integerList2.add(4);
+        integerList2.add(8);
+
+        for (int i : integerList1){
+            if(integerList2.contains(i))
+               continue;
+
+            System.out.println(i);
+        }
+    }
 }
